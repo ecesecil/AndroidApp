10352276ce2a693092e95d7a9030c76c37130225
Ece Seçil Akbaş <esecilakbas@gmail.com>
2014-01-28 14:33:15 +0200

    Harita eklendi

--- src/com/example/stepbystep/MainActivity.java
+++ src/com/example/stepbystep/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.stepbystep;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends FragmentActivity {
+	GoogleMap googleMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		try {
+			// Loading map
+			initilizeMap();
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
+		}
+			private void initilizeMap() {
+				if (googleMap == null) {
+					FragmentManager myFragmentManager = getSupportFragmentManager();
+					SupportMapFragment mySupportMapFragment = (SupportMapFragment) myFragmentManager
+							.findFragmentById(R.id.map);
+					googleMap = mySupportMapFragment.getMap();
 
-	
-}
+					
+					// check if map is created successfully or not
+			if (googleMap == null) {
+			Toast.makeText(getApplicationContext(),
+			"Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
+			}
+				}
+			}}
