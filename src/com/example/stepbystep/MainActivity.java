e60c1af3ad14824292f3dd735fcdfa27ea41520d
Ece Seçil Akbaş <esecilakbas@gmail.com>
2014-01-28 14:40:02 +0200

    Manuel lat,lng değerleriyler marker eklendi.

--- src/com/example/stepbystep/MainActivity.java
+++ src/com/example/stepbystep/MainActivity.java
@@ -5,10 +5,10 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.widget.Toast;
 
-
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity {
 	GoogleMap googleMap;
@@ -30,6 +30,15 @@ public class MainActivity extends FragmentActivity {
 					SupportMapFragment mySupportMapFragment = (SupportMapFragment) myFragmentManager
 							.findFragmentById(R.id.map);
 					googleMap = mySupportMapFragment.getMap();
+					// latitude and longitude
+					double latitude = 40.932481;
+					double longitude =29.163663 ;
+					 
+					// create marker
+					MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps ");
+					 
+					// adding marker
+					googleMap.addMarker(marker);
 
 					
 					// check if map is created successfully or not
